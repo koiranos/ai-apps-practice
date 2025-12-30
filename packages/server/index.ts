1a7658f45c16c12cfbcdf1e8c8321892c2796c00
Kostas Panagiotakis <panagiotakiskostas@yahoo.com>
2025-12-30 05:02:41 +0200

    Connect the frontend and backend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -11,6 +11,12 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello, world!");
 });
 
+app.get("/api/hello", (req: Request, res: Response) => {
+  res.json({
+    message: "Hello, World!",
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
