10b7c562030475f50444da0d082e8ef9f606c439
Kostas Panagiotakis <panagiotakiskostas@yahoo.com>
2026-01-01 01:13:04 +0200

    Setup Prettier

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -5,15 +5,15 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    proxy: {
-      "/api": "http://localhost:3000",
-    },
-  },
+   plugins: [react(), tailwindcss()],
+   resolve: {
+      alias: {
+         "@": path.resolve(__dirname, "./src"),
+      },
+   },
+   server: {
+      proxy: {
+         "/api": "http://localhost:3000",
+      },
+   },
 });
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -8,15 +8,15 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, world!");
+   res.send("Hello, world!");
 });
 
 app.get("/api/hello", (req: Request, res: Response) => {
-  res.json({
-    message: "Hello, World!",
-  });
+   res.json({
+      message: "Hello, World!",
+   });
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+   console.log(`Server is running on http://localhost:${port}`);
 });
