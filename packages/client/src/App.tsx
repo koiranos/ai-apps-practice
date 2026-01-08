913ce4d2e0801d71c2002bb6bfbba42800970bef
Kostas Panagiotakis <panagiotakiskostas@yahoo.com>
2026-01-08 11:18:56 +0200

    Build the frontend

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,19 +1,9 @@
-import { useEffect, useState } from "react";
-import { Button } from "./components/ui/button";
+import ChatBot from "./components/chat/ChatBot";
 
 function App() {
-   const [message, setMessage] = useState("");
-
-   useEffect(() => {
-      fetch("/api/hello")
-         .then((res) => res.json())
-         .then((data) => setMessage(data.message));
-   }, []);
-
    return (
-      <div className="p-4">
-         <p className="font-bold text-3xl">{message}</p>
-         <Button className="mt-2">Click me!</Button>
+      <div className="p-4 h-screen w-2xl">
+         <ChatBot />
       </div>
    );
 }
