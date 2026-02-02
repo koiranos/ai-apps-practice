cece3c8d132a311da28375a58296d3bcc09c503c
Kostas Panagiotakis <panagiotakiskostas@yahoo.com>
2026-02-03 01:26:53 +0200

    Store summary to database

--- packages/server/repositories/review.repository.ts
+++ packages/server/repositories/review.repository.ts
@@ -1,21 +1,39 @@
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { PrismaClient, type Review } from "../generated/prisma/client";
+import dayjs from "dayjs";
 
+const adapter = new PrismaMariaDb({
+   host: process.env.DATABASE_HOST,
+   user: process.env.DATABASE_USER,
+   password: process.env.DATABASE_PASSWORD,
+   database: process.env.DATABASE_NAME,
+   connectionLimit: 5,
+});
+const prisma = new PrismaClient({ adapter });
 export const reviewRepository = {
    async getReviews(productId: number, limit?: number): Promise<Review[]> {
-      const adapter = new PrismaMariaDb({
-         host: process.env.DATABASE_HOST,
-         user: process.env.DATABASE_USER,
-         password: process.env.DATABASE_PASSWORD,
-         database: process.env.DATABASE_NAME,
-         connectionLimit: 5,
-      });
-      const prisma = new PrismaClient({ adapter });
-
       return prisma.review.findMany({
          where: { productId },
          orderBy: { at: "desc" },
          take: limit,
+      });
+   },
+
+   storeReviewSummary(productId: number, summary: string) {
+      const now = new Date();
+      const expiresAt = dayjs().add(7, "days").toDate();
+
+      const data = {
+         content: summary,
+         expiresAt,
+         generatedAt: now,
+         productId,
+      };
+
+      return prisma.summary.upsert({
+         where: { productId },
+         create: data,
+         update: data,
       });
    },
 };
